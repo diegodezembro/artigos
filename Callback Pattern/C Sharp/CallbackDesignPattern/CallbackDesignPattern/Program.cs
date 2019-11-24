8041c2b2f7435b2a4d24ad6f706e0a45d23d4296
Diego Gazotto Dezembro <53023310+diegodezembro@users.noreply.github.com>
2019-11-24 17:55:12 -0300

    Add files via upload

---
+++ Callback Pattern/C Sharp/CallbackDesignPattern/CallbackDesignPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CallbackDesignPattern
+{
+    public delegate void Notificacao(string mensagem);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+            subject.EventoNotificacao += new Notificacao(new Observer().Notificar);
+            subject.RealizarTarefa();
+        }
+    }
+    abstract class Mediator
+    {
+        public event Notificacao EventoNotificacao;
+        protected void Notificar(string mensagem)
+        {
+            if (EventoNotificacao != null)
+            {
+                EventoNotificacao.Invoke(mensagem);
+            }
+        }
+        public abstract void RealizarTarefa();
+    }
+    class Subject : Mediator
+    {
+        public override void RealizarTarefa()
+        {
+            // TODO: Adicionar lógica...
+            base.Notificar("Notificação...");
+        }
+    }
+    class Observer
+    {
+        public void Notificar(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Callback Pattern/Visual Basic/CallbackDesignPattern/CallbackDesignPattern/Program.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Namespace CallbackDesignPattern
+    Public Delegate Sub Notificacao(ByVal mensagem As String)
+    Module Program
+        Sub Main()
+            Dim subject As Subject = New Subject()
+            AddHandler subject.EventoNotificacao, AddressOf New Observer().Notificar
+            subject.RealizarTarefa()
+        End Sub
+    End Module
+    Friend MustInherit Class Mediator
+        Public Event EventoNotificacao As Notificacao
+        Protected Sub Notificar(ByVal mensagem As String)
+            RaiseEvent EventoNotificacao(mensagem)
+        End Sub
+        Public MustOverride Sub RealizarTarefa()
+    End Class
+    Friend Class Subject
+        Inherits Mediator
+        Public Overrides Sub RealizarTarefa()
+            ' TODO: Adicionar lógica...
+            MyBase.Notificar("Notificação...")
+        End Sub
+    End Class
+    Friend Class Observer
+        Public Sub Notificar(ByVal mensagem As String)
+            Console.WriteLine(mensagem)
+            Console.ReadKey()
+        End Sub
+    End Class
+End Namespace
