157175fdc1dfd52933519f802a6bef14ac74f84b
Diego Gazotto Dezembro <53023310+diegodezembro@users.noreply.github.com>
2019-11-24 17:52:17 -0300

    Add files via upload

---
+++ Dependency Injection Pattern/C Sharp/DependencyInjectionPattern/DependencyInjectionPattern/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace DependencyInjectionPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parent parent = new Parent(new Child());
+            Parent parent = new Parent();
+            //parent.Child = new Child();
+            parent.InjetarDependencia(new Child());
+            parent.RealizarTarefa();
+        }
+    }
+    interface IParent
+    {
+        void InjetarDependencia(IChild child);
+    }
+    //class Parent
+    //{
+    //    private IChild _child;
+    //    public Parent(IChild child)
+    //    {
+    //        _child = child;
+    //    }
+    //    public void RealizarTarefa()
+    //    {
+    //        _child.RealizarTarefa();
+    //    }
+    //}
+    //class Parent
+    //{
+    //    private IChild _child;
+    //    //public Parent(IChild child)
+    //    //{
+    //    //    _child = child;
+    //    //}
+    //    public IChild Child
+    //    {
+    //        set
+    //        {
+    //            _child = value;
+    //        }
+    //    }
+    //    public void RealizarTarefa()
+    //    {
+    //        _child.RealizarTarefa();
+    //    }
+    //}
+    class Parent : IParent
+    {
+        private IChild _child;
+        public void InjetarDependencia(IChild child)
+        {
+            this._child = child;
+        }
+        public void RealizarTarefa()
+        {
+            this._child.RealizarTarefa();
+        }
+    }
+    interface IChild
+    {
+        void RealizarTarefa();
+    }
+    class Child : IChild
+    {
+        public void RealizarTarefa()
+        {
+            Console.WriteLine("Tarefa realizada!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dependency Injection Pattern/Visual Basic/DependencyInjectionPattern/DependencyInjectionPattern/Program.vb
@@ -0,0 +1,57 @@
+﻿Namespace DependencyInjectionPattern
+    Module Program
+        Sub Main()
+            'Dim parent As Parent = New Parent(New Child())
+            Dim parent As Parent = New Parent()
+            'parent.Child = New Child()
+            parent.InjetarDependencia(New Child())
+            parent.RealizarTarefa()
+        End Sub
+    End Module
+    Interface IParent
+        Sub InjetarDependencia(ByVal child As IChild)
+    End Interface
+    'Class Parent
+    '    Private _child As IChild
+    '    Sub New(ByVal child As IChild)
+    '        _child = child
+    '    End Sub
+    '    Public Sub RealizarTarefa()
+    '        _child.RealizarTarefa()
+    '    End Sub
+    'End Class
+    'Class Parent
+    '    Private _child As IChild
+    '    'Sub New(ByVal child As IChild)
+    '    '    _child = child
+    '    'End Sub
+    '    Public WriteOnly Property Child() As IChild
+    '        Set(ByVal value As IChild)
+    '            _child = value
+    '        End Set
+    '    End Property
+    '    Public Sub RealizarTarefa()
+    '        _child.RealizarTarefa()
+    '    End Sub
+    'End Class
+    Class Parent
+        Implements IParent
+        Private _child As IChild
+        Sub InjetarDependencia(ByVal child As IChild) Implements IParent.InjetarDependencia
+            Me._child = child
+        End Sub
+        Public Sub RealizarTarefa()
+            Me._child.RealizarTarefa()
+        End Sub
+    End Class
+    Interface IChild
+        Sub RealizarTarefa()
+    End Interface
+    Class Child
+        Implements IChild
+        Sub RealizarTarefa() Implements IChild.RealizarTarefa
+            Console.WriteLine("Tarefa realizada!")
+            Console.ReadKey()
+        End Sub
+    End Class
+End Namespace
