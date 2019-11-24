a68fade3797910c26d6d9cfa96378aa9a2e16e9b
Diego Gazotto Dezembro <53023310+diegodezembro@users.noreply.github.com>
2019-11-24 17:56:15 -0300

    Add files via upload

---
+++ Concorrência e Singleton/C Sharp/SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instancia1 = Singleton.Instancia;
+            Singleton instancia2 = Singleton.Instancia;
+            Singleton instancia3 = Singleton.Instancia;
+            if (instancia1.Equals(instancia2) && instancia2.Equals(instancia3))
+            {
+                Console.WriteLine("Mesma instância!");
+            }
+            Console.ReadKey();
+        }
+    }
+    //class Singleton
+    //{
+    //    private static Singleton _instancia;
+    //    private Singleton()
+    //    {
+    //        // Construtor privado para evitar instâncias.
+    //    }
+    //    public static Singleton Instancia
+    //    {
+    //        get
+    //        {
+    //            if (_instancia == null)
+    //            {
+    //                _instancia = new Singleton();
+    //            }
+    //            return _instancia;
+    //        }
+    //    }
+    //}
+    //class Singleton
+    //{
+    //    private static Singleton _instancia;
+    //    private static object _bloqueio = new object();
+    //    private Singleton()
+    //    {
+    //        // Construtor privado para evitar instâncias.
+    //    }
+    //    public static Singleton Instancia
+    //    {
+    //        get
+    //        {
+    //            if (_instancia == null)
+    //            {
+    //                lock (_bloqueio)
+    //                {
+    //                    _instancia = new Singleton();
+    //                }
+    //            }
+    //            return _instancia;
+    //        }
+    //    }
+    //}
+    //class Singleton
+    //{
+    //    private static Singleton _instancia;
+    //    private static object _bloqueio = new object();
+    //    private Singleton()
+    //    {
+    //        // Construtor privado para evitar instâncias.
+    //    }
+    //    public static Singleton Instancia
+    //    {
+    //        get
+    //        {
+    //            if (_instancia == null)
+    //            {
+    //                lock (_bloqueio)
+    //                {
+    //                    if (_instancia == null)
+    //                    {
+    //                        _instancia = new Singleton();
+    //                    }
+    //                }
+    //            }
+    //            return _instancia;
+    //        }
+    //    }
+    //}
+    class Singleton
+    {
+        private static readonly Singleton _instancia = new Singleton(); 
+        private Singleton()
+        {
+            // Construtor privado para evitar instâncias.
+        }
+        public static Singleton Instancia
+        {
+            get
+            {
+                return _instancia;
+            }
+        }
+    }
+}
---
+++ Concorrência e Singleton/Visual Basic/SingletonDesignPattern/SingletonDesignPattern/Program.vb
@@ -0,0 +1,74 @@
+﻿Namespace SingletonDesignPattern
+    Module Program
+        Sub Main()
+            Dim instancia1 As Singleton = Singleton.Instancia
+            Dim instancia2 As Singleton = Singleton.Instancia
+            Dim instancia3 As Singleton = Singleton.Instancia
+            If (instancia1.Equals(instancia2) AndAlso instancia2.Equals(instancia3)) Then
+                Console.WriteLine("Mesma instância!")
+            End If
+            Console.ReadKey()
+        End Sub
+    End Module
+    'Class Singleton
+    '    Private Shared _instancia As Singleton
+    '    Protected Sub New()
+    '        ' Construtor privado para evitar instâncias.
+    '    End Sub
+    '    Public Shared ReadOnly Property Instancia() As Singleton
+    '        Get
+    '            If (_instancia Is Nothing) Then
+    '                _instancia = New Singleton()
+    '            End If
+    '            Return _instancia
+    '        End Get
+    '    End Property
+    'End Class
+    'Class Singleton
+    '    Private Shared _instancia As Singleton
+    '    Private Shared _bloqueio As Object = New Object()
+    '    Protected Sub New()
+    '        ' Construtor privado para evitar instâncias.
+    '    End Sub
+    '    Public Shared ReadOnly Property Instancia() As Singleton
+    '        Get
+    '            If (_instancia Is Nothing) Then
+    '                SyncLock (_bloqueio)
+    '                    _instancia = New Singleton()
+    '                End SyncLock
+    '            End If
+    '            Return _instancia
+    '        End Get
+    '    End Property
+    'End Class
+    'Class Singleton
+    '    Private Shared _instancia As Singleton
+    '    Private Shared _bloqueio As Object = New Object()
+    '    Protected Sub New()
+    '        ' Construtor privado para evitar instâncias.
+    '    End Sub
+    '    Public Shared ReadOnly Property Instancia() As Singleton
+    '        Get
+    '            If (_instancia Is Nothing) Then
+    '                SyncLock (_bloqueio)
+    '                    If (_instancia Is Nothing) Then
+    '                        _instancia = New Singleton()
+    '                    End If
+    '                End SyncLock
+    '            End If
+    '            Return _instancia
+    '        End Get
+    '    End Property
+    'End Class
+    Class Singleton
+        Private Shared ReadOnly _instancia As Singleton = New Singleton()
+        Private Sub New()
+            ' Construtor privado para evitar instâncias.
+        End Sub
+        Public Shared ReadOnly Property Instancia() As Singleton
+            Get
+                Return _instancia
+            End Get
+        End Property
+    End Class
+End Namespace
